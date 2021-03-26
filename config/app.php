134e375488a99da6d99cde2bbf0c13591c4d5cf5
rajendraf5 <68588129+rajendraf5@users.noreply.github.com>
2021-03-26 13:39:00 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://rajendraf5.github.io/shefali.github.io'),
+    'url' => env('APP_URL', 'https://shefaliinteriors.herokuapp.com'),
 
-    'asset_url' => env('ASSET_URL', 'https://rajendraf5.github.io/shefali.github.io/resources/views'),
+    'asset_url' => env('ASSET_URL', 'https://shefaliinteriors.herokuapp.com/resources/views'),
 
     /*
     |--------------------------------------------------------------------------
